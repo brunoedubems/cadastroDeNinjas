2e62f6c220cde3f7eaf30e6fbc364be4763f0a5f
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-07-30 17:04:11 -0300

    feat: Coloca o lombook na classe para abstracao

--- src/main/java/br/com/brunoedubems/CadatroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/Missoes/MissoesModel.java
@@ -1,4 +1,29 @@
 package br.com.brunoedubems.CadatroDeNinjas.Missoes;
 
+import br.com.brunoedubems.CadatroDeNinjas.NinjaModel;
+import jakarta.persistence.*;
+        import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+    //Muito ninjas para uma missão
+    @OneToMany(mappedBy = "missoes")
+    private NinjaModel ninja;
+
 }
--- src/main/java/br/com/brunoedubems/CadatroDeNinjas/NinjaModel.java
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/NinjaModel.java
@@ -1,4 +1,67 @@
 package br.com.brunoedubems.CadatroDeNinjas;
 
+import br.com.brunoedubems.CadatroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table( name = "tb_cadastro")
 public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private int idade;
+
+    //um ninja tem uma unica missao, Muitos ninjas terão uma nija
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // criando a coluna da foreing key / chave estrangeira
+    private List<MissoesModel> missoes;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(Long id, String nome, String email, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
