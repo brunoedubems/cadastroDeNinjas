5ae6e370cc5c41e87dc41fdeebdbc41c550b4454
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-07-30 17:10:22 -0300

    fix: Coloca a coluna email como UNIQUE

--- src/main/java/br/com/brunoedubems/CadatroDeNinjas/NinjaModel.java
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/NinjaModel.java
@@ -2,11 +2,19 @@ package br.com.brunoedubems.CadatroDeNinjas;
 
 import br.com.brunoedubems.CadatroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table( name = "tb_cadastro")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +22,7 @@ public class NinjaModel {
 
     private String nome;
 
+    @Column(unique = true)
     private String email;
 
     private int idade;
@@ -22,46 +31,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") // criando a coluna da foreing key / chave estrangeira
     private List<MissoesModel> missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(Long id, String nome, String email, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
