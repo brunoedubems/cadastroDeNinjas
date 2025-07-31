63338e59e56ee8df7c7419098fa77005857eeeea
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-07-31 19:30:24 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/br/com/brunoedubems/CadatroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package br.com.brunoedubems.CadatroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missões Listadas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissoes(){
+        return "Missão criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissoes(){
+        return "Missão alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
 }
