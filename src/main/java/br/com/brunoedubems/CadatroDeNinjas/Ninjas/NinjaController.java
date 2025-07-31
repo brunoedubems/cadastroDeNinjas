e1cdb0ea95b6b370653f3660ef9e1c7d7fac8e80
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-07-31 19:35:58 -0300

    Feat: Altera o nome das rotas do controller dos ninjas

--- src/main/java/br/com/brunoedubems/CadatroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/Ninjas/NinjaController.java
@@ -10,19 +10,18 @@ public class NinjaController {
     public String boasVindas(){return "boas vindas :) ";}
 
     // Adicionar ninja (CREATE)
-    @PostMapping("/adicionar")
+    @PostMapping("/criar")
     public String criarNinja(){
         return "Ninja criado";
     }
     // Mostrar todos os ninjas (READ)
-    @GetMapping("/todos")
+    @GetMapping("/listar")
     public String mostrarTodosOsNinjas(){
         return "Ninja mostrar todos";
     }
 
-
-    // Mostrar ninja por id (READ)
-    @GetMapping("/todosID")
+       // Mostrar ninja por id (READ)
+    @GetMapping("/listarID")
     public String mostrarTodosOsNinjasPorId(){
         return "Mostrar ninja por id";
     }
