1aad59272ded4e20807016341b945c75d5450782
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-08-03 16:06:24 -0300

    doc: Adiciona Swagger na minha aplicação para a documentação das minhas rotas de controller de ninjas

--- src/main/java/br/com/brunoedubems/CadatroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/Ninjas/NinjaController.java
@@ -1,5 +1,9 @@
 package br.com.brunoedubems.CadatroDeNinjas.Ninjas;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +23,18 @@ public class NinjaController {
     }
 
     @GetMapping("/boasvindas")
+    @Operation(summary = "Mensagem de boas vindas",description = "Essa rota da uma mensagem de boas vindas")
     public String boasVindas() {
         return "boas vindas :) ";
     }
 
     // Adicionar ninja (CREATE)
     @PostMapping("/criar")
+        @Operation(summary = "Cria um novo ninja", description = "Rota cria um novo ninja e inseri no banco de dados")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "201", description = "Ninja Criado com Sucesso"),
+                @ApiResponse(responseCode = "400", description = "Erro na criação do ninja")
+        })
     public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja) {
 
         NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
@@ -44,6 +54,11 @@ public class NinjaController {
 
     // Mostrar ninja por id (READ)
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista o ninja por id", description = "Rota lista ninjas por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja encontrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Ninja não encontrado")
+    })
     public ResponseEntity<?> mostrarTodosOsNinjasPorId(@PathVariable Long id) {
         NinjaDTO ninja = ninjaService.listarNinjasPorId(id);
         if (ninja != null) {
@@ -56,7 +71,16 @@ public class NinjaController {
 
     // Alterar dados dos ninjas (UPDATE)
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<?> alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaDTO) {
+    @Operation(summary = "Altera o ninja por id", description = "Rota altera ninja por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Ninja não encontrado, não foi possível alterar")
+    })
+    public ResponseEntity<?> alterarNinjaPorId(
+            @Parameter(description = "Usuario manda o id no caminho da requisição")
+            @PathVariable Long id,
+            @Parameter(description = "Usuário manda os dados do ninja pela requisição")
+            @RequestBody NinjaDTO ninjaDTO) {
         NinjaDTO ninja = ninjaService.atualizaNinja(id, ninjaDTO);
 
         if (ninja != null) {
---
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/Ninjas/NinjaControllerUI.java
@@ -0,0 +1,28 @@
+package br.com.brunoedubems.CadatroDeNinjas.Ninjas;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
+public class NinjaControllerUI {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUI(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/listar")
+    public String mostrarTodosOsNinjas(Model model) {
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas", ninjas);
+        return "listarNinjas";
+    }
+}
