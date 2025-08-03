967e82c54a1e3476306b2ee4236564a4fdc66630
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-08-03 09:41:19 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/br/com/brunoedubems/CadatroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/br/com/brunoedubems/CadatroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,25 @@
 package br.com.brunoedubems.CadatroDeNinjas.Ninjas;
 
+import br.com.brunoedubems.CadatroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private MissoesModel missoes;
+    private String rank;
+
+
 }
